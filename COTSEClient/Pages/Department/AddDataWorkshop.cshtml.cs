0e09d065b0725b96254f5ac491d95219b2ce219a
taidmhe150076 <taidmhe150076@fpt.edu.vn>
2024-03-26 00:44:01 +0700

    set up environment , config
    set up environment , config

--- COTSEClient/Pages/Department/AddDataWorkshop.cshtml.cs
+++ COTSEClient/Pages/Department/AddDataWorkshop.cshtml.cs
@@ -1,10 +1,20 @@
+using BusinessLogic.IRepository;
+using BusinessLogic.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography.X509Certificates;
 
 namespace COTSEClient.Pages.Department
 {
     public class AddDataWorkshopModel : PageModel
     {
+        private readonly IRepositoryParticipants _repositoryParticipants;
+
+        public AddDataWorkshopModel(IRepositoryParticipants repositoryParticipants)
+        {
+            _repositoryParticipants = repositoryParticipants;
+        }
+
         public void OnGet()
         {
         }
