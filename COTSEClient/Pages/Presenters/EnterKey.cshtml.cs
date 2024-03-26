b6497f3c45f54000cf96b0b3f7a4bbc1b3159a27
taidmhe150076 <taidmhe150076@fpt.edu.vn>
2024-03-26 12:44:37 +0700

    update Presenter Pages
    update Presenter Pages

---
+++ COTSEClient/Pages/Presenters/EnterKey.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COTSEClient.Pages.Presenters
+{
+    public class EnterKeyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
