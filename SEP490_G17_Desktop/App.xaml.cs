1c0e73b1a6143558ae27a75bb1825952b4bb4ee5
tungnb2001 <lethanhtung312001@gmail.com>
2024-03-28 01:04:54 +0700

    add base

---
+++ SEP490_G17_Desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SEP490_G17_Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
