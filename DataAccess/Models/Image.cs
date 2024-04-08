e86b987265f965e252a22f7e79c60942cc5431d2
taidmhe150076 <taidmhe150076@fpt.edu.vn>
2024-04-09 00:05:51 +0700

    update Models
    update Models

---
+++ DataAccess/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public string? Image1 { get; set; }
+
+    public virtual ICollection<SlideWorkShop> SlideWorkShops { get; set; } = new List<SlideWorkShop>();
+}
