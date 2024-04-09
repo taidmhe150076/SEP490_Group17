3434e561a9485fe436ac9177480f0b106ed3231f
tungnb2001 <lethanhtung312001@gmail.com>
2024-04-09 14:46:40 +0700

    Revert "Delete BussinessLogic directory"

---
+++ BussinessLogic/IRepository/IRepositoryGPACapstone.cs
@@ -0,0 +1,16 @@
+﻿using LiveCharts.Defaults;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace BusinessLogic.IRepository
+{
+    public interface IRepositoryGPACapstone
+    {
+        List<GPAScoreRange> GetValueGPACapstoneByMajorAndYear(string major, int year);
+    }
+}
---
+++ BussinessLogic/Repository/RepositoryValidateFile.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLogic.IRepository;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Repository
+{
+
+    public class RepositoryValidateFile : IRepositoryValidateFile
+    {
+        void IRepositoryValidateFile.CloseExcelProcesses(string filePath)
+        {
+            try
+            {
+                var fileName = Path.GetFileName(filePath);
+                var test = System.Diagnostics.Process.GetProcessesByName("EXCEL");
+                foreach (var process in System.Diagnostics.Process.GetProcessesByName("EXCEL"))
+                {
+                    if (process.MainWindowTitle.Contains(fileName))
+                    {
+                        process.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error closing Excel processes: " + ex.Message);
+            }
+        }
+    }
+}
