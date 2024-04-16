052502ae3eb7b15beb199daf608f71a38590d758
Minh Hoàng <98410811+HoangpmHE153655@users.noreply.github.com>
2024-04-16 18:50:10 +0700

    update dashboard

---
+++ COTSEClient/Pages/Systems/AdminDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COTSEClient.Pages.Systems
+{
+    public class AdminDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
