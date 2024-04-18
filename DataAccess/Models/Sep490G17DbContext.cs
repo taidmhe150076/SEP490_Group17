874c86712af6f90270ccdf515b69209d0835d3f4
taidmhe150076 <taidmhe150076@fpt.edu.vn>
2024-04-18 14:17:16 +0700

    Update config
    Update config

--- DataAccess/Models/Sep490G17DbContext.cs
+++ DataAccess/Models/Sep490G17DbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Models;
 
@@ -66,8 +67,13 @@ public partial class Sep490G17DbContext : DbContext
     public virtual DbSet<WorkshopSurveyUrl> WorkshopSurveyUrls { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-LJLCRRI;database=SEP490_G17_DB;uid=sa;pwd=123;TrustServerCertificate=true;");
+    {
+        var builder = new ConfigurationBuilder()
+                     .SetBasePath(Directory.GetCurrentDirectory())
+                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfigurationRoot configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
